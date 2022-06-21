e1aa1cde41c79a13f6d5e9e47f90306fa3a7827e
Andrei M <mosneaga8@gmail.com>
2022-06-21 14:44:21 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Andrei Mosneag on 21.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
