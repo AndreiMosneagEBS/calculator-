c770a7883e7ce792c12529b611defb61e07a3017
Andrei M <mosneaga8@gmail.com>
2022-06-30 12:09:40 +0300

    Final Commit

---
+++ Calculator/New Group/TableViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  TableViewCell.swift
+//  Calculator
+//
+//  Created by Andrei Mosneag on 23.06.2022.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    static let identifier = "TableViewCell"
+    
+    @IBOutlet private weak var numbersCalculate: UILabel!
+    @IBOutlet private weak var resultat: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+//        resetView()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        selectionStyle = .none
+//        resetView()
+    }
+    
+//    private func resetView() {
+//        numbersCalculate.text = nil
+//        resultat.text = nil
+//    }
+    
+    struct Params {
+        var numbers: String
+        var results: String
+    }
+    
+    func setup(param: Params) {
+        numbersCalculate.text = param.numbers
+        resultat.text = param.results
+    }
+}
